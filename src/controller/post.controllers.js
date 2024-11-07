2b1cdcefdfb05e7ac6b8caa26efdb38994f8f1a0
Roy test <roypolocarrillo@gmail.com>
2024-11-07 17:15:50 -0500

    plan de amortizacion

--- src/controller/post.controllers.js
+++ src/controller/post.controllers.js
@@ -5,28 +5,31 @@ Mto = monto
 rate = valor interes
 Npla = numero de plazo
 Vcap = valor capital
+Sfinal = saldo final
+Sini = saldo inicial
 */
 
 export const Formula = async (req, res) => {
   const { Mto, Vint, Npla } = req.body;
   let Vcta = Math.round((Mto * Vint * (1 + Vint) ** Npla) / ((1 + Vint) ** Npla - 1));
-  let rate = Mto * Vint;
-  let Vcap = Vcta - rate;
+  let Sini = Mto;
+  let Amortizacion = [];
+  for (let i = 1; i <= Npla; i++) {
+    const rate = Sini * Vint;
+    const Vcap = Vcta - rate;
+    Sini -= Vcap;
+    Amortizacion = [...Amortizacion, {
+      Mes: i,
+      capital: Vcap,
+      interes: rate,
+      saldo_final: Sini
+    }];
+  }
   res.send({
     Vcta,
-    rate,
-    Vcap
-  }) 
+    amortizacion: Amortizacion
+  })
 }
 
-
-
-
-
-const planAmortizacion = (Npla) => {
-  for(let i = 0; i<Npla; i++){
-    
-  }
-}
 
 
